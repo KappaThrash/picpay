d6f1135c05842047e7dd56be36cd7a7f836353c0
Kappa <danielkuhin@gmail.com>
2026-05-11 18:55:47 -0300

    x

--- src/test/java/bank/picpay/controller/TransacaoControllerTest.java
+++ src/test/java/bank/picpay/controller/TransacaoControllerTest.java
@@ -1,22 +1,41 @@
 package bank.picpay.controller;
 
+import bank.picpay.models.transacao.TransacaoDTO;
+import bank.picpay.service.TransacaoService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(TransacaoController.class)
 class TransacaoControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private TransacaoService transacaoService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
-    void transacaoPost() {
+    void transacaoPost() throws Exception{
+        TransacaoDTO transacaoDTO
+
+        mockMvc.perform(post("/transfer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)).andExpect(status().isCreated())
+
+        );
     }
 }
